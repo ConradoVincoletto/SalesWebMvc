eddf60adaf2a5b98ce85e7b637c315376a4cb816
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2022-12-23 17:50:25 -0300

    Implementing simple search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -3,18 +3,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
-        public IActionResult Index()
+        private readonly SalesRecordService _salexRecordService;
+
+        public SalesRecordsController(SalesRecordService salexRecordService)
         {
+            _salexRecordService = salexRecordService;
+        }
+
+        public IActionResult Index()
+        {            
             return View();
         }
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salexRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
         public IActionResult GroupingSearch()
         {
--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -10,12 +10,12 @@ namespace SalesWebMvc.Services
 {
     public class DepartmentService
     {
-        private readonly SalesWebMvcContext _context;
+            private readonly SalesWebMvcContext _context;
 
-        public DepartmentService(SalesWebMvcContext context)
-        {
-            _context = context;
-        }
+            public DepartmentService(SalesWebMvcContext context)
+            {
+                _context = context;
+            }
 
         public async Task<List<Department>> FindAllAsync()
         {
